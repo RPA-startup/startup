6bc393e2f39f27b89a786f4247e3dbac69719408
2339086 <2339086@hyundaidept.com>
2024-08-16 14:01:40 +0900

    [feat] s3 api 적용

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-# 가상환경 생성 (venv은 가상환경 이름)
-# python3 -m venv venv
-# 가상환경 활성화 (윈도우)
-# venv\Scripts\activate
-
-from database import connect_db, get_empty_content_rows, update_content, disconnect_db
-from openai_client import process_images_and_get_response
+from database_api import connect_db, get_empty_keywords_rows, update_content, disconnect_db
+from s3_api import get_file_content_as_base64
+from openai_api import process_images_and_get_response
+import config
 
 # Main execution
 if __name__ == "__main__":
-    print("***프로그램 시작***")
+    print("*** 프로그램 시작 ***")
 
     # 데이터베이스 연결
     conn, cur = connect_db()
 
-    # Content가 비어 있는 행들 가져오기
-    rows = get_empty_content_rows(cur)
-    
-    print(rows)
+    # Keywords가 비어 있는 행들 가져오기
+    rows = get_empty_keywords_rows(cur)
     
     if rows:
         for row in rows:
@@ -25,11 +20,17 @@ if __name__ == "__main__":
 
             print(f"현재 처리 중인 행 EventID: {event_id}, 이미지 URL: {image_url}")
 
-            # OpenAI API를 사용하여 이미지 처리 및 Content 업데이트
-            response_text = process_images_and_get_response([image_url])
-            update_content(cur, conn, event_id, response_text)
+            # S3에서 이미지 가져와서 Base64로 인코딩
+            s3_file_path = image_url  # 여기서 image_url이 S3의 파일 경로임을 가정합니다.
+            bucket_name = config.s3_Info["bucket_name"]
+            base64_image = get_file_content_as_base64(bucket_name, s3_file_path)
+            
+            if base64_image:
+                # OpenAI API를 사용하여 이미지 처리 및 Content 업데이트
+                response_text = process_images_and_get_response([base64_image])
+                update_content(cur, conn, event_id, response_text)
     
     # 데이터베이스 연결 종료
     disconnect_db(conn, cur)
 
-    print("프로그램 종료.")
+    print("*** 프로그램 종료 ***")
